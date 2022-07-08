d5291d0282f14e9866235ca5693935fcabd7a19d
leonardo <leo.carvalholkp@gmail.com>
2022-07-08 17:10:23 -0300

    test: add and find user and check its existence

--- src/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.spec.ts
+++ src/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.spec.ts
@@ -8,4 +8,15 @@ describe('In memory User repository', () => {
     const user = await userRepo.findUserByEmail('any@mail.com')
     expect(user).toBeNull()
   })
+
+  it('should return if it is found in the repository', async () => {
+    const users: UserData[] = []
+    const name = 'any_name'
+    const email = 'any@mail.com'
+    const userRepo = new InMemoryUserRepository(users)
+    await userRepo.add({ name, email })
+    const user = await userRepo.findUserByEmail(email)
+    expect(user).toBeDefined()
+    expect(user.name).toBe(name)
+  })
 })
