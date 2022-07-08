320fd97c0609718c529c979311527eacdb160447
leonardo <leo.carvalholkp@gmail.com>
2022-07-08 15:40:46 -0300

    test: should add user with complete data to mailing list

---
+++ src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
@@ -0,0 +1,14 @@
+import { UserData } from './user-data'
+
+describe('Register user on mailing list use case', () => {
+  it('should add user with complete data to mailing list', async () => {
+    const users: UserData[] = []
+    const repo: UserRepository = new InMemoryUserRepository(users)
+    const useCase: RegisterUserOnMailingList = new RegisterUserOnMailingListUseCase(repo)
+    const name = 'any_name'
+    const email = 'any@email.com'
+    const response = await usecase.registerUserOnMailingList({ name, email })
+    const user = repo.findUserByEmail(email)
+    expect((await user).name).toBe(name)
+  })
+})
