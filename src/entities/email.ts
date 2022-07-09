462099199907e85a17c4d3efe42b670ff21fc4f6
leonardo <leo.carvalholkp@gmail.com>
2022-07-09 14:59:28 -0300

    feat: validate emails with empty domain

--- src/entities/email.ts
+++ src/entities/email.ts
@@ -3,7 +3,7 @@ export class Email {
     if (!email) return false
     if (email.length > 320) return false
     const [local, domain] = email.split('@')
-    if (domain.length > 255) return false
+    if (domain.length > 255 || domain.length === 0) return false
     if (local.length > 64 || local.length === 0) return false
     return true
   }
