857f3afb92b9d2db193410ce79e49bce77f6a567
leonardo <leo.carvalholkp@gmail.com>
2022-07-09 15:07:06 -0300

    feat: validate emails with domain parts large than 63 chars

--- src/entities/email.ts
+++ src/entities/email.ts
@@ -5,6 +5,10 @@ export class Email {
     const [local, domain] = email.split('@')
     if (domain.length > 255 || domain.length === 0) return false
     if (local.length > 64 || local.length === 0) return false
+    const domailParts = domain.split('.')
+    if (domailParts.some(function (part) {
+      return part.length > 63
+    })) return false
     return true
   }
 }
