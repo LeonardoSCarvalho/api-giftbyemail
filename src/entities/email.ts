626bd6cad2ec86f8b648156eb250de19d84b5895
leonardo <leo.carvalholkp@gmail.com>
2022-07-09 14:44:25 -0300

    feat: validate emails with more than 320 chars

--- src/entities/email.ts
+++ src/entities/email.ts
@@ -1,6 +1,7 @@
 export class Email {
   static validade (email: string): boolean {
     if (!email) return false
+    if (email.length > 320) return false
     const [local] = email.split('@')
     if (local.length > 64) return false
     return true
