3864271197a3f6eafcc4bb07e397a8e239b0b882
leonardo <leo.carvalholkp@gmail.com>
2022-07-10 11:26:44 -0300

    feat: register valid user to mailing list

--- src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
+++ src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
@@ -1,14 +1,18 @@
-import { UserData } from './user-data'
+import { UserData } from '../../entities/user-data'
+import { UserRepository } from './ports/user-repository'
+import { RegisterUserOnMailingList } from './register-user-on-mailing-list'
+import { InMemoryUserRepository } from './repository/in-memory-user-repository'
 
 describe('Register user on mailing list use case', () => {
   it('should add user with complete data to mailing list', async () => {
     const users: UserData[] = []
     const repo: UserRepository = new InMemoryUserRepository(users)
-    const useCase: RegisterUserOnMailingList = new RegisterUserOnMailingListUseCase(repo)
+    const usecase: RegisterUserOnMailingList = new RegisterUserOnMailingList(repo)
     const name = 'any_name'
     const email = 'any@email.com'
     const response = await usecase.registerUserOnMailingList({ name, email })
     const user = repo.findUserByEmail(email)
+    expect(response.value.name).toBe(name)
     expect((await user).name).toBe(name)
   })
 })
