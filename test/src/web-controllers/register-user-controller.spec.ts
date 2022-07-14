3ce71f2e4c7db684a8dfa0d1dc7ad885c29095e3
leonardo <leo.carvalholkp@gmail.com>
2022-07-13 21:08:59 -0300

    test: return status code 400 if name and email is invalid

--- test/src/web-controllers/register-user-controller.spec.ts
+++ test/src/web-controllers/register-user-controller.spec.ts
@@ -1,4 +1,5 @@
 import { UserData } from '@/entities'
+import { InvalidEmailError, InvalidNameError } from '@/entities/errors'
 import { RegisterUserOnMailingList } from '@/usecases/register-user-on-mailing-list'
 import { UserRepository } from '@/usecases/register-user-on-mailing-list/ports'
 import { HttpRequest, HttpResponse } from '@/web-controllers/ports'
@@ -20,5 +21,37 @@ describe('Register web controller', () => {
     const response: HttpResponse = await controller.handle(request)
     expect(response.statusCode).toBe(201)
     expect(response.body).toEqual(request.body)
+  })
+
+  it('sould return status code 400 when request contains invalid name', async () => {
+    const requestWithInvalidName: HttpRequest = {
+      body: {
+        name: 'A',
+        email: 'any@mail.com'
+      }
+    }
+    const users: UserData[] = []
+    const repo: UserRepository = new InMemoryUserRepository(users)
+    const usecase: RegisterUserOnMailingList = new RegisterUserOnMailingList(repo)
+    const controller: RegisterUserController = new RegisterUserController(usecase)
+    const response: HttpResponse = await controller.handle(requestWithInvalidName)
+    expect(response.statusCode).toBe(400)
+    expect(response.body).toBeInstanceOf(InvalidNameError)
+  })
+
+  it('sould return status code 400 when request contains invalid email', async () => {
+    const requestWithInvalidEmail: HttpRequest = {
+      body: {
+        name: 'Aanyname',
+        email: '@mail.com'
+      }
+    }
+    const users: UserData[] = []
+    const repo: UserRepository = new InMemoryUserRepository(users)
+    const usecase: RegisterUserOnMailingList = new RegisterUserOnMailingList(repo)
+    const controller: RegisterUserController = new RegisterUserController(usecase)
+    const response: HttpResponse = await controller.handle(requestWithInvalidEmail)
+    expect(response.statusCode).toBe(400)
+    expect(response.body).toBeInstanceOf(InvalidEmailError)
   })
 })
