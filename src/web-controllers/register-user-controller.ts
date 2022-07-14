eb5207cc72f01318652239e4c36ce3a7b1a8726d
leonardo <leo.carvalholkp@gmail.com>
2022-07-13 21:42:58 -0300

    feat: return status code 400 if missing user name

--- src/web-controllers/register-user-controller.ts
+++ src/web-controllers/register-user-controller.ts
@@ -1,6 +1,7 @@
 import { UserData } from '@/entities'
 import { RegisterUserOnMailingList } from '@/usecases/register-user-on-mailing-list'
 import { HttpRequest, HttpResponse } from '@/web-controllers/ports'
+import { MissingParamError } from './errors/missing-param-error'
 import { badRequest, created } from './util/http-helper'
 
 export class RegisterUserController {
@@ -10,6 +11,7 @@ export class RegisterUserController {
   }
 
   public async handle (request: HttpRequest):Promise<HttpResponse> {
+    if (!request.body.name || !request.body.email) return badRequest(new MissingParamError(request.body.name ? 'email' : 'name'))
     const userData: UserData = request.body
     const response = await this.usecase.registerUserOnMailingList(userData)
     if (response.isLeft()) {
