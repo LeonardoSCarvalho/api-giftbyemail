f21868b5dd5264028d30671c697f5dfe05de0033
leonardo <leo.carvalholkp@gmail.com>
2022-07-14 17:39:48 -0300

    feat: web controller should return 500 when server raise

--- src/web-controllers/register-user-controller.ts
+++ src/web-controllers/register-user-controller.ts
@@ -1,24 +1,28 @@
 import { UserData } from '@/entities'
-import { RegisterUserOnMailingList } from '@/usecases/register-user-on-mailing-list'
+import { UseCase } from '@/usecases/port'
 import { HttpRequest, HttpResponse } from '@/web-controllers/ports'
-import { MissingParamError } from './errors/missing-param-error'
-import { badRequest, created } from './util/http-helper'
+import { MissingParamError } from '@/web-controllers/errors/missing-param-error'
+import { badRequest, created, serverError } from '@/web-controllers/util/http-helper'
 
 export class RegisterUserController {
-  private readonly usecase: RegisterUserOnMailingList
-  constructor (usecase: RegisterUserOnMailingList) {
+  private readonly usecase: UseCase
+  constructor (usecase: UseCase) {
     this.usecase = usecase
   }
 
   public async handle (request: HttpRequest):Promise<HttpResponse> {
-    if (!request.body.name || !request.body.email) return badRequest(new MissingParamError(request.body.name ? 'email' : 'name'))
-    const userData: UserData = request.body
-    const response = await this.usecase.registerUserOnMailingList(userData)
-    if (response.isLeft()) {
-      return badRequest(response.value)
-    }
-    if (response.isRight()) {
-      return created(response.value)
+    try {
+      if (!request.body.name || !request.body.email) return badRequest(new MissingParamError(request.body.name ? 'email' : 'name'))
+      const userData: UserData = request.body
+      const response = await this.usecase.perform(userData)
+      if (response.isLeft()) {
+        return badRequest(response.value)
+      }
+      if (response.isRight()) {
+        return created(response.value)
+      }
+    } catch (error) {
+      return serverError(error)
     }
   }
 }
