4404f30543b659ef8e8b0eda8ca924217cdaea5a
leonardo <leo.carvalholkp@gmail.com>
2022-07-14 17:29:38 -0300

    refactor: introduce use case interface

--- src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.ts
+++ src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.ts
@@ -9,7 +9,7 @@ export class RegisterUserOnMailingList {
     this.userRepo = userRepo
   }
 
-  public async registerUserOnMailingList (request: UserData):Promise<Either<InvalidNameError | InvalidEmailError, UserData>> {
+  public async perform (request: UserData):Promise<Either<InvalidNameError | InvalidEmailError, UserData>> {
     const userOrError: Either<InvalidNameError | InvalidEmailError, User> = User.create(request)
     if (userOrError.isLeft()) return left(userOrError.value)
     if (!(await this.userRepo.exists(request.email))) await this.userRepo.add(request)
