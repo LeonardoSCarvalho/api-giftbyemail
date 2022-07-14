d6d94fd4f93318f16196eabbcd7e23799f7f5e39
leonardo <leo.carvalholkp@gmail.com>
2022-07-13 21:40:06 -0300

    fix: missing param error message

--- src/web-controllers/errors/missing-param-error.ts
+++ src/web-controllers/errors/missing-param-error.ts
@@ -1,5 +1,4 @@
 export class MissingParamError extends Error {
-  public readonly message: string = 'MissingParamError'
   constructor (param: string) {
     super(`Missing parameter from request: ${param}`)
   }
