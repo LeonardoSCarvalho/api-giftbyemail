e895d99423f92017ef1c60bbe3c8e3cddd3fd3e0
leonardo <leo.carvalholkp@gmail.com>
2022-07-15 16:55:02 -0300

    feat: apply cors enabling middleware with json

--- src/main/config/middleware.ts
+++ src/main/config/middleware.ts
@@ -1,5 +1,7 @@
 import { Express } from 'express'
-import { bodyPareser } from './middleware/body-parse'
+import { bodyPareser } from '@/main/config/middleware/body-parse'
+import { cors } from '@/main/config/middleware/cors'
 export default (app: Express): void => {
   app.use(bodyPareser)
+  app.use(cors)
 }
