5f24ac09efe8afb99aca48182f0d20dbae12e631
leonardo <leo.carvalholkp@gmail.com>
2022-07-16 14:07:32 -0300

    refactor: move middleware and adapter folder

--- src/main/config/middleware.ts
+++ src/main/config/middleware.ts
@@ -1,5 +1,5 @@
 import { Express } from 'express'
-import { cors, bodyPareser, contentType } from '@/main/config/middleware/'
+import { cors, bodyPareser, contentType } from '@/main/middleware'
 
 export default (app: Express): void => {
   app.use(bodyPareser)
